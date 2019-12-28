10c53ec143766d382ea989d1207ca64a5cbac935
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2019-12-28 12:43:36 -0800

    added code that connects to db

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import { config } from 'dotenv';
+import mongoose from 'mongoose';
 
 config();
 const app = express();
@@ -10,6 +11,14 @@ const y: string = 'hey';
 
 app.get('/', (req, res) => {
   res.send('The sedulous hyena ate the antelope!');
+});
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}, (err) => {
+  if (err) console.error(err);
+  else console.log('Connected to db...');
 });
 
 app.listen(port, (err) => {
